b20333f39b0d43825ffba73025aa90330da67aea
Jarno Elonen <elonen@iki.fi>
2023-03-25 22:49:33 +0200

    Make loggging use local time zone

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use docopt::Docopt;
 use std::path::PathBuf;
 use anyhow::bail;
-
+mod log;
 
 const USAGE: &'static str = r#"
 Clapshot server - backend of a video annotation tool
@@ -72,35 +72,13 @@ fn main() -> anyhow::Result<()>
     let poll_interval = args.get_str("--poll").parse::<f32>().unwrap_or(3.0);
     let resubmit_delay = poll_interval * 5.0;
 
-    let log_to_stdout = log_file == "" || log_file == "-";
-    let (log_writer, _guard) = if log_to_stdout {
-            tracing_appender::non_blocking(std::io::stdout())
-        } else {
-            let f = std::fs::OpenOptions::new().create(true).append(true).open(log_file)?;
-            tracing_appender::non_blocking(f)
-        };
-
     // Setup logging
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", if debug {"debug,clapshot_server=debug"} else {"info,clapshot_server=info"});
-    };
-    let log_sbsc = tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_file(false)
-        .with_line_number(false)
-        .with_thread_ids(false)
-        .with_target(false)
-        //.pretty() // for debugging
-        .with_writer(log_writer)
-        .with_ansi(log_to_stdout);
-
-    //let log_sbsc = if json_log { log_sbsc.json().finish() } else { log_sbsc.finish() };
-    if json_log {
-        tracing::subscriber::set_global_default(log_sbsc.json().finish())
-    } else {
-        tracing::subscriber::set_global_default(log_sbsc.finish())
-    }.expect("tracing::subscriber::set_global_default failed");
-
+    let time_offset = time::UtcOffset::current_local_offset().expect("should get local offset");
+    let _log_guard = log::setup_logging(
+        time_offset,
+        debug,
+        &log_file,
+        json_log);
 
     clapshot_server::run_clapshot(data_dir, migrate, url_base, port, n_workers, target_bitrate, poll_interval, resubmit_delay)
 }
