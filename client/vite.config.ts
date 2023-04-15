0d8c0c29ec3448798ea8b36119b23efc87065cd3
Jarno Elonen <elonen@iki.fi>
2023-04-16 00:28:14 +0300

    Fail build on  typescript errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess';
+import Checker from 'vite-plugin-checker'
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({preprocess: [sveltePreprocess({ typescript: true })]}),
+    Checker({ typescript: true }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
