783e9b8cb5d71b901bc9fa5de7edbb62143936d1
elonen <elonen@iki.fi>
2024-05-20 12:18:00 +0300

    Fix bad migration script that cascade-deleted existing comments

--- server/migrations/2024-05-13-093800_add_users_table/up.sql
+++ server/migrations/2024-05-13-093800_add_users_table/up.sql
@@ -27,8 +27,11 @@ INSERT OR IGNORE INTO users (id, name)
 SELECT DISTINCT user_id, user_name FROM comments WHERE user_name IS NOT NULL;
 
 
--- Step 3: Modify the videos table
--- Remove the `user_name` column, and make `user_id` a foreign key column
+-- Step 3: Create new videos, comments and messages tables
+
+-- !! Need to keep the old tables for now, so cascade rules don't delete anything before we've copied data from all the tables !!
+
+-- Videos -- Remove the `user_name` column, and make `user_id` a foreign key column
 ALTER TABLE videos DROP COLUMN user_name;
 CREATE TABLE videos_new (
         id VARCHAR NOT NULL,
@@ -45,23 +48,14 @@ CREATE TABLE videos_new (
         thumb_sheet_rows INTEGER,
         PRIMARY KEY (id)
 );
-INSERT INTO videos_new SELECT * FROM videos;
-DROP TABLE videos;
-ALTER TABLE videos_new RENAME TO videos;
 
-CREATE INDEX ix_video_user_id ON videos (user_id);
-CREATE INDEX ix_video_added_time ON videos (added_time);
-
-
--- Step 4: Modify the comments table
--- Rename user_name into username_ifnull, make current user_id a NULLABLE foreign key
+-- Comments -- Rename user_name into username_ifnull, make current user_id a NULLABLE foreign key
 -- Unlike videos, this allows comments without login, and to keep comments even if the user is deleted
-
 ALTER TABLE comments RENAME COLUMN user_name TO username_ifnull;
 CREATE TABLE comments_new (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    video_id VARCHAR NOT NULL REFERENCES videos(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    parent_id INTEGER REFERENCES comments(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    video_id VARCHAR NOT NULL REFERENCES videos_new(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    parent_id INTEGER REFERENCES comments_new(id) ON UPDATE CASCADE ON DELETE CASCADE,
     created DATETIME DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
     edited DATETIME,
     user_id user_id VARCHAR(255) NULL REFERENCES users (id) ON DELETE SET NULL ON UPDATE CASCADE,
@@ -70,38 +64,51 @@ CREATE TABLE comments_new (
     timecode VARCHAR,
     drawing VARCHAR
 );
-INSERT INTO comments_new SELECT * FROM comments;
-DROP TABLE comments;
-ALTER TABLE comments_new RENAME TO comments;
 
-CREATE INDEX ix_comments_user_id ON comments (user_id);
-CREATE INDEX ix_comments_parent_id ON comments (parent_id);
-
--- When users are deleted, update their name to all their comments from now on
-CREATE TRIGGER tr_comments_set_username_on_user_delete AFTER DELETE ON users
-BEGIN
-    UPDATE comments SET username_ifnull = OLD.name WHERE user_id = OLD.id;
-END;
-
-
--- Step 5: Modify the messages table
--- Make user_id a foreign key
-
+-- Message -- Make user_id a foreign key
 CREATE TABLE messages_new (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     user_id VARCHAR(255) NOT NULL REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     created DATETIME DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
     seen BOOLEAN NOT NULL,
     video_id VARCHAR,  -- keep messages about videos even if it's deleted, so no foreign key
-    comment_id INTEGER REFERENCES comments (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comments_new (id) ON UPDATE CASCADE ON DELETE CASCADE,
     event_name VARCHAR NOT NULL,
     message VARCHAR NOT NULL,
     details VARCHAR NOT NULL
 );
+
+-- Step 4: Populate the new tables
+INSERT INTO videos_new SELECT * FROM videos;
+INSERT INTO comments_new SELECT * FROM comments;
 INSERT INTO messages_new SELECT * FROM messages;
+
+
+-- Step 5: Turn the _new tables into the main tables
 DROP TABLE messages;
 ALTER TABLE messages_new RENAME TO messages;
+
+DROP TABLE comments;
+ALTER TABLE comments_new RENAME TO comments;
+
+DROP TABLE videos;
+ALTER TABLE videos_new RENAME TO videos;
+
+
+-- Step 6: Add indexes and triggers
+
+CREATE INDEX ix_comments_user_id ON comments (user_id);
+CREATE INDEX ix_comments_parent_id ON comments (parent_id);
 
 CREATE INDEX ix_messages_user_id ON messages (user_id);
 CREATE INDEX ix_messages_comment_id ON messages (comment_id);
 CREATE INDEX ix_messages_video_id ON messages (video_id);
+
+CREATE INDEX ix_video_user_id ON videos (user_id);
+CREATE INDEX ix_video_added_time ON videos (added_time);
+
+-- When users are deleted, update their name to all their comments from now on
+CREATE TRIGGER tr_comments_set_username_on_user_delete AFTER DELETE ON users
+BEGIN
+    UPDATE comments SET username_ifnull = OLD.name WHERE user_id = OLD.id;
+END;
